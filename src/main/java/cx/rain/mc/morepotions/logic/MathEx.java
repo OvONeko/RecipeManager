cc0e21a11781f55dd1f7fd0692e443e3f5b1b731
Elihuso Quigley <Elihuso@outlook.com>
2023-12-18 11:35:04 +0800

    [+] logic

---
+++ src/main/java/cx/rain/mc/morepotions/logic/MathEx.java
@@ -0,0 +1,13 @@
+package cx.rain.mc.morepotions.logic;
+
+public class MathEx {
+    public static double Mapping(double x, double a1, double b1, double a2, double b2) {
+        if (x < a1)
+            throw new IllegalArgumentException("x must ≥ a₁");
+        return ((x - a1) * (b2 - a2)) / (b1 - a1) + a2;
+    }
+
+    public static int IntegerMapping(int x, int a1, int b1, int a2, int b2) throws IllegalArgumentException {
+        return (int) Mapping(x, a1, b1, a2, b2);
+    }
+}
